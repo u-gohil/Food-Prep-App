9ed857c1487ff08e407ccccfc2a981c5e6d8ae83
756D6D61 <756D6D61@gmail.com>
2019-11-23 13:15:18 +0000

    add async

--- src/App.vue
+++ src/App.vue
@@ -33,14 +33,21 @@ export default {
         return error;
       }
     },
-    addEmployee(employee) {
-      const lastId =
-        this.employees.length > 0
-          ? this.employees[this.employees.length - 1].id
-          : 0;
-      const id = lastId + 1;
-      const newEmployee = { ...employee, id };
-      this.employees = [...this.employees, newEmployee];
+    async addEmployee(employee) {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users',
+          {
+            method: 'POST',
+            body: JSON.stringify(employee),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' }
+          }
+        );
+        const data = await response.json();
+        this.employees = [...this.employees, data];
+      } catch (error) {
+        return error;
+      }
     },
     deleteEmployee(id) {
       this.employees = this.employees.filter(
@@ -63,22 +70,6 @@ export default {
     this.getEmployees();
   },
 
-  async addEmployee(employee) {
-    try {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/users',
-        {
-          method: 'POST',
-          body: JSON.stringify(employee),
-          headers: { 'Content-type': 'application/json; charset=UTF-8' }
-        }
-      );
-      const data = await response.json();
-      this.employees = [...this.employees, data];
-    } catch (error) {
-      return error;
-    }
-  },
   async editEmployee(id, updatedEmployee) {
     try {
       const response = await fetch(
